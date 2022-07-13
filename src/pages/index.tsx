44c13c01f99163344c9b980a202c189eb7cf2114
João Vitor F <joaovitor.jobs2001@gmail.com>
2022-07-13 10:59:50 -0300

    feat: added get all topics functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,23 @@ import { Header } from '../components/Header'
 import { Nav } from '../components/Nav'
 import { Topic } from '../components/Topic'
 import { Aside } from '../components/Aside'
+import axios from 'axios'
 
-const Home: NextPage = () => {
+interface TopicProps {
+  _id: string
+  author: {
+    id: string
+    displayName: string
+    photoURL: string
+  }
+  title: string
+  body: string
+  createdAt: string
+  upvotes: string
+  downvotes: string
+}
+
+const Home: NextPage = ({ data }: any) => {
   return (
     <div>
       <Head>
@@ -17,14 +32,25 @@ const Home: NextPage = () => {
         <div className="max-w-7xl mx-auto py-6 grid lg:grid-cols-[1fr_768px] xl:grid-cols-[1fr_768px_1fr] sm:gap-6">
           <Nav className="hidden lg:inline-block" />
           <main className="flex flex-col gap-6">
-            <Topic />
-            <Topic />
+            {data.topics.map((topic: TopicProps) => (
+              <Topic key={topic._id} topic={topic} />
+            ))}
           </main>
           <Aside />
         </div>
       </div>
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const response = await axios.get('http://localhost:3000/api/getAllTopics')
+
+  return {
+    props: {
+      data: response.data,
+    },
+  }
 }
 
 export default Home
